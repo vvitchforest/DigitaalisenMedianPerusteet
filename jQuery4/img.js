9c4bb5a12659fb1e1b8a4a7dd3f01bb4493f1959
irina <irina.konovalova455@gmail.com>
2020-09-01 12:06:28 +0300

    Example solution added

--- jQuery4/img.js
+++ jQuery4/img.js
@@ -27,10 +27,30 @@ $(function() {
 
     $('img').click(function() {
           $(this).hide();
-          $('ul').append('<li>' + this.id +'</li>');
+          $('ul').append($('<li>' + this.id +'</li>'));
         },
     );
   });
 });
+
+
+/*Esimerkkiratkaisu*/
+/*
+$(function() {
+  const valitseVari = function(evt) {
+    $(this).hide();
+    const vari = $(this).attr('src').split('.')[0];
+    $('ul').append($('<li>' + vari + '</li>'));
+  };
+
+  $('button').click(function() {
+    $('#advice').html('select your favourite colour among the colours');
+    $('#img1').attr('src', 'red.jpg').click(valitseVari);
+    $('#img2').attr('src', 'blue.jpg').click(valitseVari);
+    $('#img3').attr('src', 'yellow.jpg').click(valitseVari);
+    $('#sel').html('your favourite colours in order');
+  });
+});
+ */
 
 
