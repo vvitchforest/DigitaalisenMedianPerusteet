d9875dd0a27ff04a352d3c3c8211a52c97692ccd
irina <irina.konovalova455@gmail.com>
2020-09-05 15:31:31 +0300

    jQueryAjax done

--- jQueryAjax/ajax.js
+++ jQueryAjax/ajax.js
@@ -1,15 +1,38 @@
 $(function() {
-  $('#search-button').click(function(){
-    $.getJSON('http://api.tvmaze.com/search/shows?q=girls', function(data){
-      //console.log(data);
-      /*for(let i = 0; i < data.length; i+) {
-        console.log(data[i])
-      } */
+  $('#search-button').click(function() {
+    $('li').each(function() {
+      $(this).remove();
+    });
 
-      $.each(data, function(index, value){
+    let input = $('#input').val();
+
+    $.getJSON('http://api.tvmaze.com/search/shows?q=' + input, function(data) {
+
+      $.each(data, function(index, value) {
         console.log(index, value);
+        let $li = $('<li></li>');
+        let $article = $('<article></article>');
+
+        $article.append(value.show.name);
+        if (value.show.genres !== null) {
+          $article.append('<br>' + value.show.genres);
+        }
+        if (value.show.officialSite !== null) {
+          $article.append('<br><a href=' + value.show.officialSite +
+              '>Linkki kotisivulle</a>');
+        }
+        if (value.show.image !== null) {
+          $article.append(
+              '<br><br><img src=' + value.show.image['medium'] + '>');
+        }
+        if (value.show.summary !== null) {
+          $article.append('<br>' + value.show.summary);
+        }
+
+        $($li).append($article);
+        $('ul').append($li);
+
       });
     });
   });
-
 });
