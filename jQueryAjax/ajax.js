4a3362f714fd3b727cf61b781e6438884c2e9ad8
irina <irina.konovalova455@gmail.com>
2020-09-25 19:05:58 +0300

    ajax refactoring

--- jQueryAjax/ajax.js
+++ jQueryAjax/ajax.js
@@ -1,36 +1,44 @@
 $(function() {
   $('#search-button').click(function() {
-    $('li').each(function() {
-      $(this).remove();
-    });
 
     let input = $('#input').val();
 
     $.getJSON('http://api.tvmaze.com/search/shows?q=' + input, function(data) {
+      $('section').empty();
 
       $.each(data, function(index, value) {
         console.log(index, value);
-        let $li = $('<li></li>');
-        let $article = $('<article></article>');
 
-        $article.append(value.show.name);
-        if (value.show.genres !== null) {
-          $article.append('<br>' + value.show.genres);
-        }
-        if (value.show.officialSite !== null) {
-          $article.append('<br><a href=' + value.show.officialSite +
-              '>Linkki kotisivulle</a>');
-        }
-        if (value.show.image !== null) {
-          $article.append(
-              '<br><br><img src=' + value.show.image['medium'] + '>');
-        }
-        if (value.show.summary !== null) {
-          $article.append('<br>' + value.show.summary);
+        let link;
+        let image;
+        let summary;
+
+        if (value.show.officialSite) {
+          link = value.show.officialSite;
+        } else
+          link = value.show.url;
+
+        if (value.show.image) {
+          image = value.show.image['medium'];
+        } else {
+          image = '';
         }
 
-        $($li).append($article);
-        $('ul').append($li);
+        if (value.show.summary) {
+          summary = value.show.summary;
+        } else
+          summary = '';
+
+        const item = `<article>
+                        <h2>${value.show.name}</h2>
+                        <h5>${value.show.genres.join('&nbsp;')}</h5>
+                        <a href = ${link} target = "_blank">Official site</a><br><br>
+                        <img src =  ${image}>
+                        <p>${summary}</p>
+
+</article> `;
+
+        $('section').append($(item));
 
       });
     });
